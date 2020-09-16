40cb1d69147c4db860c824213f6a442ddbe1d0f0
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-16 15:29:36 +0200

    Connectionstring implemented

--- pra.DBConnected.CORE/Helper.cs
+++ pra.DBConnected.CORE/Helper.cs
@@ -8,6 +8,12 @@ namespace Pra.DBConnected.CORE
 {
     public class Helper
     {
+        public static string GetConnectionString()
+        {
+            // PAS ONDERSTAANDE CONNECTIESTRING AAN
+
+            return @"Data Source=DESKTOP-O3H1S4C\SQLEXPRESS;Initial Catalog = Bibliotheek; Integrated security = true;";
+        }
         public static string HandleQuotes(string waarde)
         {
             return waarde.Trim().Replace("'", "''");
